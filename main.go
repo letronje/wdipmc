575c78cad16b2cd05890106945fe9e9aa4fa3b87
Manoj Mathai <manoj.mathai@grabtaxi.com>
2019-09-24 00:08:24 +0800

    Test lib to convert svy21 to (lat,lon)

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/letronje/wdipmc/svy21"
 )
 
 const port = 80
@@ -12,9 +15,20 @@ func main() {
 	r := gin.Default()
 
 	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		c.JSON(http.StatusOK, gin.H{"user": user})
+	r.GET("/latlon", func(c *gin.Context) {
+		north, _ := c.GetQuery("north")
+		northf, _ := strconv.ParseFloat(north, 64)
+
+		east, _ := c.GetQuery("east")
+		eastf, _ := strconv.ParseFloat(east, 64)
+
+		lat, lon := svy21.ToLatLon(northf, eastf)
+		fmt.Println(lat, lon)
+
+		c.JSON(
+			http.StatusOK, gin.H{
+				"latlon": fmt.Sprintf("%f,%f", lat, lon),
+			})
 	})
 
 	r.Run(fmt.Sprintf(":%d", port))
